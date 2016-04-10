8f35cdc17cb941ce3a9a5d31b100da7a892f4bd4
zewillze <you_youzzw@sina.com>
2016-04-10 22:37:42 +0800

    add all files

---
+++ app/src/main/java/com/will/zengzewill/mobilecodeinfo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.will.zengzewill.mobilecodeinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView numberText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
